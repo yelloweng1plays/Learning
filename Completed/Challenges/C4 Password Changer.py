002c24bb5ac13aac7a7c4d1d268a4014175ddd0c
yelloweng1plays <70171620+yelloweng1plays@users.noreply.github.com>
2022-05-10 21:27:59 +0100

    Update C4 Password Changer.py

--- Completed/Challenges/C4 Password Changer.py
+++ Completed/Challenges/C4 Password Changer.py
@@ -1,6 +1,19 @@
-from getpass import getpass
+from getpass4 import getpass
 
-Password1 = getpass("Input new password: ")
+debug = True
+
+Password1 = getpass("Input new password: ") 
 print("Please confirm your password")
 Password2 = getpass("Input new password: ")
 
+if debug: print("Pass1: ",Password1," Pass2: ",Password2)
+
+if Password1 == Password2:
+    if Password1.islower():
+        print("Your password must contain at least 1 uppercase and 1 lowercase letter")
+    elif Password1.isupper():
+        print("Your password must contain at least 1 uppercase and 1 lowercase letter")
+    else:
+        print("Congratulations your new password has been set!")
+else:
+    print("Error: Your passwords do not match!")
